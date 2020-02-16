6119e08ac353e22f9c258f8a6223dc4546f23284
Lingwei Gu <gulw2001@163.com>
2020-02-15 20:29:52 -0500

    Add files via upload

--- planner/forms.py
+++ planner/forms.py
@@ -5,21 +5,27 @@ import json
 
 
 class SearchForm(forms.Form):
-    city = forms.CharField(label="city", max_length=80)
-    date_from = forms.DateField(label="date_from")
-    date_to = forms.DateField(label="date_to")
+    city = forms.CharField(max_length=80)
+    date_from = str(forms.DateTimeField(input_formats=['%d/%m/%Y %H:%M']))
+    date_to = str(forms.DateTimeField(input_formats=['%d/%m/%Y %H:%M']))
     min_budget = forms.IntegerField(min_value=1, max_value=4)
     max_budget = forms.IntegerField(min_value=1, max_value=4)
     keywords = forms.CharField(max_length=100)
+    @property
+    def min_budg(self):
+        return self.min_budget + 0
+    @property
+    def max_budg(self):
+        return self.min_budget + 0
 
     def __str__(self):
         return f"{self.city}:\n from {self.date_from} to {self.date_to},\n budget range = {self.min_budget} ~ {self.max_budget},\n keywords = {self.keywords}\n"
 
     def makeAPICall(self):
         if self.is_valid():
-            DataGatherer.get_location_data(self.city, min_budget, max_budget, "restaurant")
-            DataGatherer.get_location_data(self.city, min_budget, max_budget, "lodging")
-            DataGatherer.get_event_data(self.city, date_from, date_to, keywords)
+            DataGatherer.get_location_data(self.city, self.min_budg(), self.max_budg(), "restaurant")
+            DataGatherer.get_location_data(self.city, self.min_budg(), self.max_budg(), "lodging")
+            DataGatherer.get_event_data(self.city, self.date_from, self.date_to, self.keywords)
             try:
                 with open("restaurant_data.json", "r") as read_file:
                     json.load(read_file)
