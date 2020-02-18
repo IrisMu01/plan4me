f3b2db5219a55ecd503ea3d5b3c65499cfc8a449
Iris Mu <5215lingyim@gmail.com>
2020-02-18 13:35:01 -0500

    second time formatting result

--- planner/res_json_parsar.py
+++ planner/res_json_parsar.py
@@ -2,17 +2,19 @@ import json
 def location_parser(fileName):
     with open(fileName, "r") as read_file:
         location_dict = json.load(read_file)
-    tidy_dict = {}
+    tidy_list = []
     entry_num = 0
     for entry in location_dict["results"]:
-        tidy_dict.update({entry_num: [
-            {"address": entry["formatted_address"]},
-            {"name": entry["name"]},
-            {"phote": entry["photos"][0]["html_attributions"]},
-            {"rating": entry["rating"]},
-            {"how_many_rating": entry["user_ratings_total"]}]})
+        tidy_dict = {}
+        tidy_dict.update({"index": "Result #" + str((entry_num+1)) + ":"})
+        tidy_dict.update({"address": entry["formatted_address"]})
+        tidy_dict.update({"name": entry["name"]})
+        #tidy_dict.update({"photo": entry["photos"][0]["html_attributions"]})
+        tidy_dict.update({"rating": entry["rating"]})
+        tidy_dict.update({"how_many_ratings": entry["user_ratings_total"]})
         if "price_level" in entry:
-            tidy_dict[entry_num].append({"price_level": entry["price_level"]})
+            tidy_dict.update({"price_level": entry["price_level"]})
+        tidy_list.append(tidy_dict)
         entry_num += 1
-    return tidy_dict
+    return tidy_list
 
